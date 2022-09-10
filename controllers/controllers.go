55de898a35e2a8e5e736bd632c521efc0cb6f112
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-10 23:27:43 +0530

    jwt token generated

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/VJ-Vijay77/JWT_Auth_With_Fiber/db"
 	"github.com/VJ-Vijay77/JWT_Auth_With_Fiber/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 )
+
+const JWTsecret = "secret"
 
 func Home(c *fiber.Ctx) error {
 	return c.JSON("Welcome to Home")
@@ -51,10 +56,28 @@ func Login(c *fiber.Ctx) error {
 	}
 
 
-	claims := jwt.NewWithClaims()
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256,jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Minute *2).Unix(),
+	})
+
+	token,err := claims.SignedString([]byte(JWTsecret))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message":"could not login",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Minute *2),
+	}
+	c.Cookie(&cookie)
 
 	return c.JSON(fiber.Map{
 		"status":"Login Success",
-		"user":user,
+		"token":token,
 	})
 }
